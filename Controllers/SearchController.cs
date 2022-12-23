464e33c4216cdc6f060ef5da96a845b1f9f0a2f8
phamquanghien <pqh.cntt@gmail.com>
2022-12-23 12:56:12 +0700

    Done

--- Controllers/SearchController.cs
+++ Controllers/SearchController.cs
@@ -1,5 +1,8 @@
+using System.Security.Principal;
 using Microsoft.AspNetCore.Mvc;
 using JavaExamFinal.Data;
+using Microsoft.EntityFrameworkCore;
+using JavaExamFinal.Models;
 
 namespace JavaExamFinal.Controllers
 {
@@ -38,6 +41,121 @@ namespace JavaExamFinal.Controllers
             }
             ViewBag.thongBao = message;
             return View();
+        }
+        public async Task<IActionResult> Student()
+        {
+            return View(await _context.Student.Take(20).ToListAsync());
+        }
+        [HttpPost]
+        public async Task<IActionResult> Student(string stdID)
+        {
+            if(!string.IsNullOrEmpty(stdID))
+            {
+                return View(await _context.Student.Where(m => m.StudentID == stdID || m.Subject == stdID || m.Subject == stdID).ToListAsync());
+            }
+            ModelState.AddModelError("","Vui lòng nhập mã sinh viên để tìm kiếm thông tin");
+            return View();
+        }
+        // GET: Student/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Student/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(string Security, Student student)
+        {
+            if(string.IsNullOrEmpty(Security)) {
+                ModelState.AddModelError("", "Mã xác thực không được để trống");
+            } else if(Security != "14022012") {
+                ModelState.AddModelError("", "Mã xác thực không chính xác");
+            }
+            else {
+                student.IsActive = true;
+                student.FullName = student.FirstName + " " + student.LastName;
+                ModelState.AddModelError("", "Học phần: " + student.Subject + "-" + student.IsActive + "-" + student.FullName);
+                if (ModelState.IsValid)
+                {
+                    student.ID = Guid.NewGuid();
+                    _context.Add(student);
+                    await _context.SaveChangesAsync();
+                    return RedirectToAction(nameof(Index));
+                }
+            }
+            return View(student);
+        }
+        public async Task<IActionResult> Edit(Guid? id)
+        {
+            if (id == null || _context.Student == null)
+            {
+                return NotFound();
+            }
+
+            var student = await _context.Student.FindAsync(id);
+            if (student == null)
+            {
+                return NotFound();
+            }
+            return View(student);
+        }
+
+        // POST: Student/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Guid id, string SecurityCode, [Bind("ID,StudentID,FirstName,LastName,FullName,SubjectGroup,Subject,IsActive")] Student student)
+        {
+            if (id != student.ID)
+            {
+                return NotFound();
+            }
+            if(string.IsNullOrEmpty(SecurityCode)) {
+                ModelState.AddModelError("", "Mã xác thực không được để trống");
+            } else
+            {
+                if(SecurityCode != "14022012") {
+                    ModelState.AddModelError("", "Mã xác thực không chính xác");
+                }
+                else {
+                    if (ModelState.IsValid)
+                    {
+                        try
+                        {
+                            _context.Update(student);
+                            var dktByStudentID = await _context.DangKyThi.Where(m => m.StudentID == student.StudentID).ToListAsync();
+                            foreach(var item in dktByStudentID)
+                            {
+                                item.FirstName = student.FirstName;
+                                item.LastName = student.LastName;
+                                item.FullName = student.FullName;
+                            }
+                            await _context.SaveChangesAsync();
+                        }
+                        catch (DbUpdateConcurrencyException)
+                        {
+                            if (!StudentExists(student.ID))
+                            {
+                                return NotFound();
+                            }
+                            else
+                            {
+                                throw;
+                            }
+                        }
+                        return RedirectToAction(nameof(Index));
+                    }
+                }
+            }
+            return View(student);
+        }
+        private bool StudentExists(Guid id)
+        {
+          return (_context.Student?.Any(e => e.ID == id)).GetValueOrDefault();
         }
     }
 }
